35407ad48ff2d8db95884ba0f6cd1e73f3f01f26
Max Prokhorov <prokhorov.max@outlook.com>
2019-08-01 09:40:57 +0300

    remove _address, store directly in the ctx

--- code/espurna/sensors/PingSensor.h
+++ code/espurna/sensors/PingSensor.h
@@ -78,6 +78,7 @@ struct ping_ctx_t {
     uint16_t id = PING_ID;
     uint16_t delay = PING_DELAY;
 
+    uint16_t seq_timeout = 2000;
     uint16_t seq_retries = PING_RETRIES;
     uint16_t seq_fail = 0;
     uint16_t seq_num = 0;
@@ -88,31 +89,14 @@ struct ping_ctx_t {
     struct raw_pcb *pcb;
 };
 
-/** Prepare a echo ICMP request */
-void ping_prepare_echo (struct ping_ctx_t *ctx, struct icmp_echo_hdr *iecho, u16_t len) {
-
-    size_t i;
-    size_t data_len = len - sizeof(struct icmp_echo_hdr);
-    ICMPH_TYPE_SET(iecho, ICMP_ECHO);
-    ICMPH_CODE_SET(iecho, 0);
-    iecho->chksum = 0;
-    iecho->id     = ctx->id;
-    iecho->seqno  = lwip_htons(++ctx->seq_num);
-
-    /* fill the additional data buffer with some data */
-    for(i = 0; i < data_len; i++) {
-        ((char*)iecho)[sizeof(struct icmp_echo_hdr) + i] = (char)i;
-    }
-
-    iecho->chksum = inet_chksum(iecho, len);
-}
-
-/* Ping using the raw ip */
+/* LWIP callback for recv. Will trigger when ICMP answer is received */
 #if LWIP_VERSION_MAJOR == 1
 u8_t ping_recv(void* arg, struct raw_pcb* pcb, struct pbuf* p, ip_addr_t* addr) {
 #else
 u8_t ping_recv(void* arg, struct raw_pcb* pcb, struct pbuf* p, const ip_addr_t* addr) {
 #endif
+
+    DEBUG_MSG("[PING] ping_recv\n");
 
     struct ping_ctx_t* ctx = (struct ping_ctx_t*)arg;
 
@@ -122,25 +106,85 @@ u8_t ping_recv(void* arg, struct raw_pcb* pcb, struct pbuf* p, const ip_addr_t* 
     LWIP_UNUSED_ARG(addr);
     LWIP_ASSERT("p != NULL", p != NULL);
 
-    if ((iecho->id == ctx->id) && (iecho->seqno == lwip_htons(ctx->seq_num))) {
-        iecho = (struct icmp_echo_hdr*)((long)p->payload + PBUF_IP_HLEN);
-        if (iecho->id == ctx->id)
-        {
-            ctx->state = ping_ctx_t::OK;
+    // STABLE-1_4_0: if (pbuf_header( p, -PBUF_IP_HLEN)==0) {
+    DEBUG_MSG("[PING] ping_recv pbuf tot_len=%u expected=%u\n", p->tot_len, (PBUF_IP_HLEN + sizeof(struct icmp_echo_hdr)));
+    if ((p->tot_len >= (PBUF_IP_HLEN + sizeof(struct icmp_echo_hdr)))
+    #if LWIP_VERSION_MAJOR == 1
+            && (pbuf_header(p, -PBUF_IP_HLEN) == 0)
+    #else
+            && (pbuf_remove_header(p, PBUF_IP_HLEN) == 0)
+    #endif
+        ) {
+        iecho = (struct icmp_echo_hdr *)p->payload;
+        DEBUG_MSG("[PING] ping_recv iecho id=%x seqno=%u expected=%u\n", iecho->id, lwip_ntohs(iecho->seqno), ctx->seq_num);
+        if ((iecho->id == ctx->id) && (iecho->seqno == lwip_htons(ctx->seq_num))) {
+            DEBUG_MSG("[PING] ping_recv OK\n");
+            ctx->state = ping_ctx_t::IDLE;
             --ctx->seq_fail;
             pbuf_free(p);
             return 1; /* eat the packet */
         }
+        #if LWIP_VERSION_MAJOR == 1
+            pbuf_header(p, PBUF_IP_HLEN);
+        #else
+            pbuf_add_header(p, PBUF_IP_HLEN);
+        #endif
     }
+
+    DEBUG_MSG("[PING] ping_recv fail\n");
 
     return 0; /* don't eat the packet */
 }
 
+/* Fill in all required fields for iecho struct */
+void ping_prepare_echo(ping_ctx_t *ctx, struct icmp_echo_hdr *iecho, u16_t len) {
+
+    size_t data_len = len - sizeof(struct icmp_echo_hdr);
+
+    ICMPH_TYPE_SET(iecho, ICMP_ECHO);
+    ICMPH_CODE_SET(iecho, 0);
+
+    iecho->chksum = 0;
+    iecho->id     = ctx->id;
+    iecho->seqno  = lwip_htons(ctx->seq_num);
+
+    /* fill the additional data buffer with some data */
+    for(int i = 0; i < data_len; i++) {
+        ((char*)iecho)[sizeof(struct icmp_echo_hdr) + i] = (char)i;
+    }
+
+    iecho->chksum = inet_chksum(iecho, len);
+}
+
 #define PING_DATA_SIZE 32
 
-void ping_send(struct ping_ctx_t *ctx) {
+/* Periodically called, send when available and process ping timeout */
+void ping_periodic(struct ping_ctx_t *ctx) {
 
-    if (ctx->target.addr == IPADDR_ANY) return;
+    DEBUG_MSG("[PING] ping_periodic \n");
+
+    // lwip2 note: ip_route(src, dest) instead of ip_route(dest), and the code below will work anyways without this check
+    #if LWIP_VERSION_MAJOR == 1
+    {
+        netif* interface = ip_route(&ctx->target);
+        if (!interface) {
+            DEBUG_MSG("[PING] No route to %s\n", ipaddr_ntoa(&ctx->target));
+            return;
+        }
+    }
+    #endif
+
+    if (ctx->state == ping_ctx_t::SENT) {
+        // bail out after timeout
+        if (ctx->time - sys_now() > ctx->seq_timeout) {
+            DEBUG_MSG("[PING] ping timeout\n");
+            ctx->state = ping_ctx_t::IDLE;
+            return;
+        }
+        // don't try to send multiple requests
+        DEBUG_MSG("[PING] ping_periodic waiting until IDLE\n");
+        return;
+    }
 
     struct pbuf *p;
     struct icmp_echo_hdr *iecho;
@@ -148,22 +192,30 @@ void ping_send(struct ping_ctx_t *ctx) {
 
     p = pbuf_alloc(PBUF_IP, ping_size, PBUF_RAM);
     if (!p) {
+        DEBUG_MSG("[PING] ping_periodic pbuf_alloc failed\n");
         return;
     }
 
-    if ((p->len == p->tot_len) && (p->next == NULL)) {
+    if ((p->len == p->tot_len) && (p->next == nullptr)) {
+        // lwip1 XXX: this was moved from inside of ping_prepare_echo
+        // lwip_htons is a one-line macro, increment happens twice
+        ++ctx->seq_fail;
+        ++ctx->seq_num;
+
         iecho = (struct icmp_echo_hdr *)p->payload;
         ping_prepare_echo(ctx, iecho, (u16)ping_size);
-        DEBUG_MSG("[PING] ping_send to=%s seq=%u fail=%u size=%u\n",
-            ipaddr_ntoa(&ctx->target), ctx->seq_num, ctx->seq_fail, ping_size);
-        ++ctx->seq_fail;
-        ctx->time = sys_now();
+
+        DEBUG_MSG("[PING] ping_periodic raw_send to=%s seq_num=%u size=%u\n",
+            ipaddr_ntoa(&ctx->target), ctx->seq_num, ping_size);
         raw_sendto(ctx->pcb, p, &ctx->target);
+
+        ctx->time = sys_now();
+        ctx->state = ping_ctx_t::SENT;
     }
     pbuf_free(p);
 }
 
-void ping_send_and_wait(struct ping_ctx_t*);
+void ping_send_and_wait(ping_ctx_t*);
 
 void ping_cb(void* arg) {
 
@@ -172,16 +224,14 @@ void ping_cb(void* arg) {
 
 }
 
-void ping_send_and_wait(struct ping_ctx_t *ctx) {
-    ctx->state = ping_ctx_t::SENT;
-    ping_send(ctx);
+void ping_send_and_wait(ping_ctx_t *ctx) {
+    ping_periodic(ctx);
     sys_timeout(ctx->delay, ping_cb, ctx);
 }
 
-void ping_raw_init(ping_ctx_t *ctx, const ip_addr_t* ping_addr) {
+void ping_raw_init(ping_ctx_t *ctx) {
     if (ctx->state == ping_ctx_t::STOPPED) {
         ctx->pcb = raw_new(IP_PROTO_ICMP);
-        ip_addr_copy(ctx->target, *ping_addr);
         raw_recv(ctx->pcb, ping_recv, ctx);
         raw_bind(ctx->pcb, IP_ADDR_ANY);
         sys_timeout(ctx->delay, ping_cb, ctx);
@@ -213,15 +263,15 @@ class PingSensor : public BaseSensor {
         // ---------------------------------------------------------------------
 
         const ip_addr_t& getAddress() {
-            return _address;
+            return ping_ctx.target;
         }
 
         void setAddress(const String& address) {
-            ipaddr_aton(address.c_str(), &_address);
+            ipaddr_aton(address.c_str(), &ping_ctx.target);
         }
 
         void setAddress(const ip_addr_t& address) {
-            ip_addr_copy(_address, address);
+            ip_addr_copy(ping_ctx.target, address);
         }
 
         bool useGatewayIP() {
@@ -249,10 +299,11 @@ class PingSensor : public BaseSensor {
         //      in theory, this would allow ipv6 support out-of-the-box
         //      Right now, use SDK API to directly reference netif gw
         void updateGatewayIP() {
+            DEBUG_MSG("[PING] updateGatewayIP() enter\n");
             _address_set = true;
             struct netif* sta = eagle_lwip_getif(STATION_IF);
-            ip_addr_copy(_address, sta->gw);
-            DEBUG_MSG("[PING] Updating target=%s gw=%s\n", ipaddr_ntoa(&_address), ipaddr_ntoa(&sta->gw));
+            DEBUG_MSG("[PING] Updating target=%s gw=%s\n", ipaddr_ntoa(&ping_ctx.target), ipaddr_ntoa(&sta->gw));
+            ip_addr_copy(ping_ctx.target, sta->gw);
         }
 
         // ---------------------------------------------------------------------
@@ -263,14 +314,15 @@ class PingSensor : public BaseSensor {
         void begin() {
             if (!wifiConnected()) return;
             if (!_address_set) return;
-            ping_raw_init(&ping_ctx, &_address);
+            DEBUG_MSG("[PING] Ready\n");
+            ping_raw_init(&ping_ctx);
             _ready = true;
         }
 
         // Descriptive name of the sensor
         String description() {
             char buffer[20];
-            snprintf(buffer, sizeof(buffer), "PING @ %s", ipaddr_ntoa(&_address));
+            snprintf(buffer, sizeof(buffer), "PING @ %s", ipaddr_ntoa(&ping_ctx.target));
             return String(buffer);
         }
 
@@ -281,7 +333,8 @@ class PingSensor : public BaseSensor {
 
         // Address of the sensor (it could be the GPIO or I2C address)
         String address(unsigned char index) {
-            return ipaddr_ntoa(&_address);
+            UNUSED(index);
+            return ipaddr_ntoa(&ping_ctx.target);
         }
 
         // Type for slot # index
@@ -321,12 +374,16 @@ class PingSensor : public BaseSensor {
             setDelay(getSetting("pingDelay", PING_DELAY).toInt());
             setID(getSetting("pingID", PING_ID).toInt());
             setRetries(getSetting("pingRetries", PING_RETRIES).toInt());
+            DEBUG_MSG("[PING] configure() seq_retries=%u delay=%u id=%x\n", ping_ctx.seq_retries, ping_ctx.delay, ping_ctx.id);
 
             String addr = getSetting("pingAddress", PING_ADDRESS);
             if (addr.toInt() == 0) {
+                DEBUG_MSG("[PING] Using GW address\n");
                 useGatewayIP(true);
             } else {
+                DEBUG_MSG("[PING] Using %s\n", addr.c_str());
                 setAddress(addr);
+                _address_set = true;
             }
         }
 
@@ -337,7 +394,7 @@ class PingSensor : public BaseSensor {
         // ---------------------------------------------------------------------
 
         ping_ctx_t ping_ctx;
-        ip_addr_t _address;
+
         bool _use_gateway_ip = false;
         bool _address_set = false;
 
